238e1977b24c728f2028a396efe1b7841eeaccc8
DesignerWPM <designerwpmtech@gmail.com>
2025-02-12 16:53:02 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
+const CACHE_NAME = 'delfin-jp-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  'https://unpkg.com/lucide@latest'
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado.');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
